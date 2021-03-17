8153f6f28e3d09c00233dc1ca139b2ff7f08771a
Oliver Wiegers <o.wiegers@syseleven.de>
2021-03-17 07:15:09 +0100

    Fix paths in install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 if [ "$(command -v stow)" ]; then
     stow tmux
 else
-    ln -s "$HOME/personal_tmux/tmux/.tmux.conf" "$HOME/"
-    ln -s "$HOME/personal_tmux/tmux/.tmux.conf.local" "$HOME/"
-    ln -s "$HOME/personal_tmux/tmux/.tmux.conf.remote" "$HOME/"
+    ln -s "$HOME/.tmuxist/tmux/.tmux.conf" "$HOME/"
+    ln -s "$HOME/.tmuxist/tmux/.tmux.conf.local" "$HOME/"
+    ln -s "$HOME/.tmuxist/tmux/.tmux.conf.remote" "$HOME/"
 fi
