083e09e1e8e44564f427524eb38d38a92353d7e0
oliverwiegers <oliver.wiegers@netlogix.de>
2025-04-08 14:57:27 +0200

    feat: Make colors work and update flake

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
 
         devShells = {
           default = pkgs.mkShell {
-            NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
+            NIX_CONFIG = "extra-experimental-features = nix-command flakes";
             nativeBuildInputs = with pkgs; [
               git
               pre-commit
